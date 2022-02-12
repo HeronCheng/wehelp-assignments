87a0bb52721806552ef002615a73acaac9ec62ae
HeronCheng <95325263+HeronCheng@users.noreply.github.com>
2022-02-12 20:44:08 +0800

    Add files via upload

--- week-6/signin.py
+++ week-6/signin.py
@@ -6,23 +6,22 @@ Signin=Blueprint('Signin',__name__)
 @Signin.route("/signin",methods=["POST"])
 def signin():
     connection=mysql.connector.connect(host='localhost',
-                                      port='3306',
-                                      user='root',
-                                      password='123456',
-                                      database='website',)
+                                       port='3306',
+                                       user='root',
+                                       password='123456',
+                                       database='website',)
     cursor=connection.cursor()
-    cursor.execute('SELECT `username` FROM `member`;')
-    accountName=cursor.fetchall()
-    cursor.execute('SELECT `password` FROM `member`;')
-    passwordName=cursor.fetchall()
+    account=request.form.get("account2")
+    password=request.form.get("password2")
+    cursor.execute("SELECT `username`,`password` from `member` WHERE `username`='"+account+"' and `password`='"+password+"';")
+    checkData=cursor.fetchall()
     cursor.close()
-    connection.commit()
-    connection.close() 
-    if (request.form.get("account2"),) in accountName and (request.form.get("password2"),) in passwordName:
-        user=request.form.get("account2")
-        session["user"]=user
-        return redirect(url_for("Member.member"))  
-    elif request.form.get("account2")=="" or request.form.get("password2")=="":
-        return redirect("/error/?message=請輸入帳號、密碼")
+    connection.close()     
+    if account=="" or password=="":
+        return redirect("/error/?message=請輸入帳號、密碼")  
+    elif checkData == []:
+        return redirect("/error/?message=帳號或密碼輸入錯誤")
     else:
-        return redirect("/error/?message=帳號、或密碼輸入錯誤")
+        user=account
+        session["user"]=user
+        return redirect(url_for("Member.member"))
--- week-6/signup.py
+++ week-6/signup.py
@@ -6,32 +6,28 @@ Signup=Blueprint('Signup',__name__)
 @Signup.route('/signup',methods=["POST"])
 def signup():
     connection=mysql.connector.connect(host='localhost',
-                                      port='3306',
-                                      user='root',
-                                      password='123456',
-                                      database='website',
-                                      buffered=True,)
+                                       port='3306',
+                                       user='root',
+                                       password='123456',
+                                       database='website',
+                                       buffered=True,)
     cursor=connection.cursor()
     nameData=request.form.get("membername")
     accountData=request.form.get("account1")
     passwordData=request.form.get("password1")
-    cursor.execute('SELECT `username` FROM `member`;')
-    accountName=cursor.fetchall()
+    cursor.execute("SELECT `username` from `member` WHERE `username`='"+accountData+"';")
+    checkregisterData=cursor.fetchall()
     if nameData=="" or accountData=="" or passwordData=="":
         cursor.close()
-        connection.commit()
         connection.close()
         return redirect("/error/?message=請輸入註冊資料")
-    elif (accountData,) not in accountName:
+    elif not checkregisterData == []:
+        cursor.close()
+        connection.close() 
+        return redirect("/error/?message=帳號已經被註冊")  
+    else:
         cursor.execute("INSERT INTO `member` (`name`,`username`,`password`) SELECT'"+nameData+"','"+accountData+"','"+passwordData+"'FROM DUAL WHERE NOT EXISTS (SELECT `username` FROM `member` WHERE `username`='"+accountData+"');")
         cursor.close()
         connection.commit()
         connection.close() 
         return render_template("week6_frontpage.html")
-    else:
-        cursor.close()
-        connection.commit()
-        connection.close() 
-        return redirect("/error/?message=帳號已經被註冊")
-    
-    
